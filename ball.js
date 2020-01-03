38d4928d37335e194ea8d27bf3a89ac1e27029ff
Petar Angelov <peshoyo@gmail.com>
2020-01-03 14:13:39 +0200

    Update ball.js

--- ball.js
+++ ball.js
@@ -14,12 +14,13 @@ class Ball{
 
 	get X() { return this.x; }
 	get Y() { return this.y; }
-	//Update velocity
+	//Update velocity.
 	updateV(Hx, Hy){
 		this.vx += Hx;
 		this.vy += Hy;	
 	}
-	//Update ball coordinates
+	
+	//Update ball coordinates.
 	update(){
 		this.x += this.vx;
 		this.y += this.vy;	
@@ -32,7 +33,7 @@ class Ball{
 		
 		//Draw boxes.
 		ctx.fillStyle = '#6fa';
-      	ctx.fillRect(0, 0, W, H);
+      		ctx.fillRect(0, 0, W, H);
 		ctx.fillStyle = 'blue'
 		ctx.fillRect(W*0.3,H*0.3,150,50);
 		ctx.fillRect(W*0.3+100,H*0.3+100,150,50);
@@ -45,34 +46,35 @@ class Ball{
 		ctx.fillRect(W-10,H-40,40,40);
 		
 		//Draw ball.
-    	ctx.beginPath();
-    	ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    	ctx.stroke();
+    		ctx.beginPath();
+    		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    		ctx.stroke();
 		ctx.fill();
   	}
 };
 
-//Check whether cicle touches a line
+//Check whether cicle touches a line.
 function doesLineInterceptCircle(A, B, C, radius) {
 	var dist;
-    const v1x = B.x - A.x;
+    	const v1x = B.x - A.x;
 	const v1y = B.y - A.y;
-    const v2x = C.x - A.x;
-    const v2y = C.y - A.y;
+    	const v2x = C.x - A.x;
+    	const v2y = C.y - A.y;
     
 	// Distance to the closest point of the line from the circle center.
-    const u = (v2x * v1x + v2y * v1y) / (v1y * v1y + v1x * v1x);
+	const u = (v2x * v1x + v2y * v1y) / (v1y * v1y + v1x * v1x);
         
-    // Point is on the line
-    if(u >= 0 && u <= 1){
-    	dist  = (A.x + v1x * u - C.x) ** 2 + (A.y + v1y * u - C.y) ** 2;
-    } 
+    	// Point is on the line.
+    	if(u >= 0 && u <= 1){
+    		dist  = (A.x + v1x * u - C.x) ** 2 + (A.y + v1y * u - C.y) ** 2;
+    	} 
 	else {
-    	// Point is not on the line
-        dist = u < 0 ?
-        	(A.x - C.x) ** 2 + (A.y - C.y) ** 2 :
-            (B.x - C.x) ** 2 + (B.y - C.y) ** 2;
+    		// Point is not on the line.
+        	dist = u < 0 ?
+        		(A.x - C.x) ** 2 + (A.y - C.y) ** 2 :
+            		(B.x - C.x) ** 2 + (B.y - C.y) ** 2;
 	}
-    return dist < radius * radius;
+	
+    	return dist < radius * radius;
 }
 
